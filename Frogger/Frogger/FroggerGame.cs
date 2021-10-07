465eeb991cb3757fc648c431673d5c8011e999d7
Tobias Kattanek <tkattanek00@gmail.com>
2021-10-07 10:11:44 +0200

    Init Project

---
+++ Frogger/Frogger/FroggerGame.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frogger
+{
+    public class FroggerGame
+    {
+        public FroggerGame(int windowWidth, int windowHeight)
+        {
+            Raylib.InitWindow(windowHeight, windowHeight, "Frogger");
+        }
+
+        public void GameLoop()
+        {
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+                // Game Logic goes here
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+    }
+}
---
+++ Frogger/Frogger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Frogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FroggerGame fg = new FroggerGame(300, 300);
+            fg.GameLoop();
+        }
+    }
+}
